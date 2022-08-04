a171a535cba048262e34778516dfc6082148695f
EverSilverio <ever.silverio@gmail.com>
2022-08-04 20:20:05 -0300

    Aula 06 ajustes Serie

--- src/Aula06_Integradora/Main.java
+++ src/Aula06_Integradora/Main.java
@@ -4,12 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         iSerie s1 = new SerieProxy();
-        s1.getSerie("SOA");
+        iSerie s2 = new SerieProxy();
+
+
+        s2.getSerie("PeakyBlinders");
+        System.out.println(s2.getSerie("PeakyBlinders"));
+
         s1.getSerie("SOA");
         s1.getSerie("SOA");
         s1.getSerie("SOA");
         s1.getSerie("SOA");
         System.out.println(s1.getSerie("SOA"));
+
+
 
     }
 }
--- src/Aula06_Integradora/SerieProxy.java
+++ src/Aula06_Integradora/SerieProxy.java
@@ -5,9 +5,10 @@ public class SerieProxy implements iSerie {
 
     @Override
     public String getSerie(String nome) {
+        Serie serie = new Serie();
         setQtdViews(getQtdViews() + 1);
         if (getQtdViews() <= 5) {
-            return nome + " foi assistido " + getQtdViews() + " vezes.";
+            return serie.getSerie(nome) + " foi assistido " + getQtdViews() + " vezes.";
         } else {
             throw new SerieNaoHabilitadaException("Deu Pobrema! Vc ja viu muitas vezes, va' lavar uma louça!");
         }
